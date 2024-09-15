6e8f9ef0916c71a46da079b055cb385e0bb4c453
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-15 14:42:47 +0700

    Add Navbar to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next'
+import React from 'react'
 import { Inter } from 'next/font/google'
 
 import './globals.css'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +19,14 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<div className='w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
+					<Navbar />
+				</div>
+				<div className=' bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
+					{children}
+				</div>
+			</body>
 		</html>
 	)
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,65 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+import MobileMenu from './MobileMenu'
+
+export default function Navbar() {
+	return (
+		<div className='h-24 flex items-center justify-between'>
+			{/* LEFT */}
+			<div className='md:hidden lg:block w-[20%]'>
+				<Link href='/' className='font-bold text-xl text-blue-600'>
+					LAMASOCIAL
+				</Link>
+			</div>
+			{/* CENTER */}
+			<div className='hidden md:flex w-[50%] text-sm items-center justify-between'>
+				{/* LINKS */}
+				<div className='flex gap-6 text-gray-600'>
+					<Link href='/' className='flex items-center gap-2'>
+						<Image
+							src='/home.png'
+							alt='Homepage'
+							width={16}
+							height={16}
+							className='w-4 h-4'
+						/>
+						<span>Home</span>
+					</Link>
+					<Link href='/' className='flex items-center gap-2'>
+						<Image
+							src='/friends.png'
+							alt='Friends'
+							width={16}
+							height={16}
+							className='w-4 h-4'
+						/>
+						<span>Friends</span>
+					</Link>
+					<Link href='/' className='flex items-center gap-2'>
+						<Image
+							src='/stories.png'
+							alt='Stories'
+							width={16}
+							height={16}
+							className='w-4 h-4'
+						/>
+						<span>Stories</span>
+					</Link>
+				</div>
+				<div className='hidden xl:flex p-2 bg-slate-100 items-center rounded-xl'>
+					<input
+						type='text'
+						placeholder='search...'
+						className='bg-transparent outline-none'
+					/>
+					<Image src='/search.png' alt='' width={14} height={14} />
+				</div>
+			</div>
+			{/* RIGHT */}
+			<div className='w-[30%] flex items-center gap-4 xl:gap-8 justify-end'>
+				<MobileMenu />
+			</div>{' '}
+		</div>
+	)
+}
