e1553275b0ecce2645ab0c1c2d68deeb3e11835e
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-16 10:46:35 +0700

    Create ProfilePage

--- src/components/right-menu/RightMenu.tsx
+++ src/components/right-menu/RightMenu.tsx
@@ -1,3 +1,4 @@
+import UserInfoCard from './UserInfoCard'
 import FriendRequests from './FriendRequests'
 import Birthdays from './Birthdays'
 import Ad from '../Ad'
@@ -5,6 +6,11 @@ import Ad from '../Ad'
 export default function RightMenu({ userId }: { userId?: string }) {
 	return (
 		<div className='flex flex-col gap-6'>
+			{userId ? (
+				<>
+					<UserInfoCard userId={userId} />
+				</>
+			) : null}
 			<FriendRequests />
 			<Birthdays />
 			<Ad size='md' />
