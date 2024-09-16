a64fc7d0bd9e3fe4cf93fe979ac291d62a342521
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-16 19:00:01 +0700

    switchFollow server action

--- src/components/right-menu/UserInfoCardInteraction.tsx
+++ src/components/right-menu/UserInfoCardInteraction.tsx
@@ -1,5 +1,9 @@
 'use client'
 
+import { useState } from 'react'
+
+import { switchFollow } from '@/lib/actions'
+
 export default function UserInfoCardInteraction({
 	userId,
 	isUserBlocked,
@@ -11,13 +15,31 @@ export default function UserInfoCardInteraction({
 	isFollowing: boolean
 	isFollowingSent: boolean
 }) {
+	const [userState, setUserState] = useState({
+		following: isFollowing,
+		blocked: isUserBlocked,
+		followingRequestSent: isFollowingSent,
+	})
+
+	const follow = async () => {
+		try {
+			await switchFollow(userId)
+			setUserState((prev) => ({
+				...prev,
+				following: prev.following && false,
+				followingRequestSent:
+					!prev.following && !prev.followingRequestSent ? true : false,
+			}))
+		} catch (err) {}
+	}
+
 	return (
 		<>
-			<form action=''>
+			<form action={follow}>
 				<button className='w-full bg-blue-500 text-white text-sm rounded-md p-2'>
-					{isFollowing
+					{userState.following
 						? 'Following'
-						: isFollowingSent
+						: userState.followingRequestSent
 						? 'Friend Request Sent'
 						: 'Follow'}
 				</button>
@@ -26,7 +48,7 @@ export default function UserInfoCardInteraction({
 			<form action='' className='self-end '>
 				<button>
 					<span className='text-red-400 text-xs cursor-pointer'>
-						{isUserBlocked ? 'Unblock User' : 'Block User'}
+						{userState.blocked ? 'Unblock User' : 'Block User'}
 					</span>
 				</button>
 			</form>
