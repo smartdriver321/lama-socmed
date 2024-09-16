575dff9f26731bc31abe7ef54e1d46cf6f33c90f
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-16 15:00:59 +0700

    Fetching user data for ProfileCard

--- src/components/left-menu/ProfileCard.tsx
+++ src/components/left-menu/ProfileCard.tsx
@@ -1,26 +1,53 @@
-import Image from 'next/image'
 import Link from 'next/link'
+import Image from 'next/image'
+import { auth } from '@clerk/nextjs/server'
 
-export default function ProfileCard() {
+import prisma from '@/lib/client'
+
+export default async function ProfileCard() {
+	const { userId } = auth()
+
+	if (!userId) return null
+
+	const user = await prisma.user.findFirst({
+		where: {
+			id: userId,
+		},
+		include: {
+			_count: {
+				select: {
+					followers: true,
+				},
+			},
+		},
+	})
+
+	if (!user) return null
+
 	return (
 		<div className='p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6'>
 			<div className='h-20 relative'>
 				<Image
-					src='https://images.pexels.com/photos/28098017/pexels-photo-28098017/free-photo-of-a-table-with-a-pink-ribbon-and-a-book-on-it.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+					src={user.cover || '/noCover.png'}
 					alt=''
 					fill
 					className='rounded-md object-cover'
 				/>
 				<Image
-					src='https://images.pexels.com/photos/18460532/pexels-photo-18460532/free-photo-of-roofs-of-medieval-buildings-in-town.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+					src={user.avatar || '/noAvatar.png'}
 					alt=''
 					width={48}
 					height={48}
 					className='rounded-full object-cover w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10'
 				/>
 			</div>
+
 			<div className='h-20 flex flex-col gap-2 items-center'>
-				<span className='font-semibold'></span>
+				<span className='font-semibold'>
+					{user.name && user.surname
+						? user.name + ' ' + user.surname
+						: user.username}
+				</span>
 				<div className='flex items-center gap-4'>
 					<div className='flex'>
 						<Image
@@ -45,9 +72,11 @@ export default function ProfileCard() {
 							className='rounded-full object-cover w-3 h-3'
 						/>
 					</div>
-					<span className='text-xs text-gray-500'>Followers</span>
+					<span className='text-xs text-gray-500'>
+						{user._count.followers} Followers
+					</span>
 				</div>
-				<Link href={`/profile/`}>
+				<Link href={`/profile/${user.username}`}>
 					<button className='bg-blue-500 text-white text-xs p-2 rounded-md'>
 						My Profile
 					</button>
