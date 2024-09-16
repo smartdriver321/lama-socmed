21a7721e0381066d26979c307b1e40006663a3dc
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-16 11:22:06 +0700

    Add ProfileCard to LeftMenu

--- src/app/(home)/profile/[id]/page.tsx
+++ src/app/(home)/profile/[id]/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import LeftMenu from '@/components/left-menu/LeftMenu'
 import Feed from '@/components/feed/Feed'
 import RightMenu from '@/components/right-menu/RightMenu'
@@ -10,11 +12,43 @@ export default function ProfilePage() {
 			</div>
 			<div className='w-full lg:w-[70%] xl:w-[50%]'>
 				<div className='flex flex-col gap-6'>
+					<div className='flex flex-col items-center justify-center'>
+						<div className='w-full h-64 relative'>
+							<Image
+								src='https://images.pexels.com/photos/28348904/pexels-photo-28348904/free-photo-of-a-wave-breaking-at-sunset-with-the-sun-behind-it.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+								alt=''
+								fill
+								className='rounded-md object-cover'
+							/>
+							<Image
+								src='https://images.pexels.com/photos/27271561/pexels-photo-27271561/free-photo-of-a-sunset-over-the-mountains-with-a-yellow-sky.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+								alt=''
+								width={128}
+								height={128}
+								className='w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover'
+							/>
+						</div>
+						<h1 className='mt-20 mb-4 text-2xl font-medium'></h1>
+						<div className='flex items-center justify-center gap-12 mb-4'>
+							<div className='flex flex-col items-center'>
+								<span className='font-medium'>123</span>
+								<span className='text-sm'>Posts</span>
+							</div>
+							<div className='flex flex-col items-center'>
+								<span className='font-medium'>1.2K</span>
+								<span className='text-sm'>Followers</span>
+							</div>
+							<div className='flex flex-col items-center'>
+								<span className='font-medium'>100</span>
+								<span className='text-sm'>Following</span>
+							</div>
+						</div>
+					</div>
 					<Feed />
 				</div>
 			</div>
 			<div className='hidden lg:block w-[30%]'>
-				<RightMenu userId='test' />
+				<RightMenu />
 			</div>
 		</div>
 	)
---
+++ src/components/left-menu/ProfileCard.tsx
@@ -0,0 +1,58 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function ProfileCard() {
+	return (
+		<div className='p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6'>
+			<div className='h-20 relative'>
+				<Image
+					src='https://images.pexels.com/photos/28098017/pexels-photo-28098017/free-photo-of-a-table-with-a-pink-ribbon-and-a-book-on-it.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+					alt=''
+					fill
+					className='rounded-md object-cover'
+				/>
+				<Image
+					src='https://images.pexels.com/photos/18460532/pexels-photo-18460532/free-photo-of-roofs-of-medieval-buildings-in-town.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+					alt=''
+					width={48}
+					height={48}
+					className='rounded-full object-cover w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10'
+				/>
+			</div>
+			<div className='h-20 flex flex-col gap-2 items-center'>
+				<span className='font-semibold'></span>
+				<div className='flex items-center gap-4'>
+					<div className='flex'>
+						<Image
+							src='https://images.pexels.com/photos/19578755/pexels-photo-19578755/free-photo-of-woman-watching-birds-and-landscape.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load'
+							alt=''
+							width={12}
+							height={12}
+							className='rounded-full object-cover w-3 h-3'
+						/>
+						<Image
+							src='https://images.pexels.com/photos/19578755/pexels-photo-19578755/free-photo-of-woman-watching-birds-and-landscape.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load'
+							alt=''
+							width={12}
+							height={12}
+							className='rounded-full object-cover w-3 h-3'
+						/>
+						<Image
+							src='https://images.pexels.com/photos/19578755/pexels-photo-19578755/free-photo-of-woman-watching-birds-and-landscape.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load'
+							alt=''
+							width={12}
+							height={12}
+							className='rounded-full object-cover w-3 h-3'
+						/>
+					</div>
+					<span className='text-xs text-gray-500'>Followers</span>
+				</div>
+				<Link href={`/profile/`}>
+					<button className='bg-blue-500 text-white text-xs p-2 rounded-md'>
+						My Profile
+					</button>
+				</Link>
+			</div>
+		</div>
+	)
+}
