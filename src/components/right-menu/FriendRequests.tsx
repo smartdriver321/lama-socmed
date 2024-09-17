737e73418266358e9f4dd9788754422ae194c2c2
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-17 12:15:17 +0700

    Add server actions: acceptFollowRequest and declineFollowRequest

--- src/components/right-menu/FriendRequests.tsx
+++ src/components/right-menu/FriendRequests.tsx
@@ -1,7 +1,25 @@
-import Image from 'next/image'
 import Link from 'next/link'
+import { auth } from '@clerk/nextjs/server'
 
-export default function FriendRequests() {
+import prisma from '@/lib/client'
+import FriendRequestList from './FriendRequestList'
+
+export default async function FriendRequests() {
+	const { userId } = auth()
+
+	if (!userId) return null
+
+	const requests = await prisma.followRequest.findMany({
+		where: {
+			receiverId: userId,
+		},
+		include: {
+			sender: true,
+		},
+	})
+
+	if (requests.length === 0) return null
+
 	return (
 		<div className='p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4'>
 			{/* TOP */}
@@ -12,69 +30,7 @@ export default function FriendRequests() {
 				</Link>
 			</div>
 			{/* USER */}
-			<div className=''>
-				<div className='flex items-center justify-between'>
-					<div className='flex items-center gap-4'>
-						<Image
-							src='https://images.pexels.com/photos/27793422/pexels-photo-27793422/free-photo-of-handsome-man-in-white-shirt-posing-by-antiques-on-street.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
-							alt=''
-							width={40}
-							height={40}
-							className='w-10 h-10 rounded-full object-cover'
-						/>
-						<span className='font-semibold'>Wayne Burton</span>
-					</div>
-
-					<div className='flex gap-3 justify-end'>
-						<Image
-							src='/accept.png'
-							alt=''
-							width={20}
-							height={20}
-							className='cursor-pointer'
-						/>
-						<Image
-							src='/reject.png'
-							alt=''
-							width={20}
-							height={20}
-							className='cursor-pointer'
-						/>
-					</div>
-				</div>
-			</div>
-
-			<div className=''>
-				<div className='flex items-center justify-between'>
-					<div className='flex items-center gap-4'>
-						<Image
-							src='https://images.pexels.com/photos/27793422/pexels-photo-27793422/free-photo-of-handsome-man-in-white-shirt-posing-by-antiques-on-street.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
-							alt=''
-							width={40}
-							height={40}
-							className='w-10 h-10 rounded-full object-cover'
-						/>
-						<span className='font-semibold'>Wayne Burton</span>
-					</div>
-
-					<div className='flex gap-3 justify-end'>
-						<Image
-							src='/accept.png'
-							alt=''
-							width={20}
-							height={20}
-							className='cursor-pointer'
-						/>
-						<Image
-							src='/reject.png'
-							alt=''
-							width={20}
-							height={20}
-							className='cursor-pointer'
-						/>
-					</div>
-				</div>
-			</div>
+			<FriendRequestList requests={requests} />
 		</div>
 	)
 }
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -138,3 +138,66 @@ export const updateProfile = async (
 		return { success: false, error: true }
 	}
 }
+
+export const acceptFollowRequest = async (userId: string) => {
+	const { userId: currentUserId } = auth()
+
+	if (!currentUserId) {
+		throw new Error('User is not Authenticated!!')
+	}
+
+	try {
+		const existingFollowRequest = await prisma.followRequest.findFirst({
+			where: {
+				senderId: userId,
+				receiverId: currentUserId,
+			},
+		})
+
+		if (existingFollowRequest) {
+			await prisma.followRequest.delete({
+				where: {
+					id: existingFollowRequest.id,
+				},
+			})
+
+			await prisma.follower.create({
+				data: {
+					followerId: userId,
+					followingId: currentUserId,
+				},
+			})
+		}
+	} catch (err) {
+		console.log(err)
+		throw new Error('Something went wrong!')
+	}
+}
+
+export const declineFollowRequest = async (userId: string) => {
+	const { userId: currentUserId } = auth()
+
+	if (!currentUserId) {
+		throw new Error('User is not Authenticated!!')
+	}
+
+	try {
+		const existingFollowRequest = await prisma.followRequest.findFirst({
+			where: {
+				senderId: userId,
+				receiverId: currentUserId,
+			},
+		})
+
+		if (existingFollowRequest) {
+			await prisma.followRequest.delete({
+				where: {
+					id: existingFollowRequest.id,
+				},
+			})
+		}
+	} catch (err) {
+		console.log(err)
+		throw new Error('Something went wrong!')
+	}
+}
