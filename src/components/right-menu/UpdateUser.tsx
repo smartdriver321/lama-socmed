0bbd4f841bdb64c424367f96a42fcccf8475c879
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-17 13:52:34 +0700

    Configure Cloudinary for uploading image

--- src/components/right-menu/UpdateUser.tsx
+++ src/components/right-menu/UpdateUser.tsx
@@ -6,11 +6,14 @@ import { useActionState, useState } from 'react'
 import { User } from '@prisma/client'
 import { updateProfile } from '@/lib/actions'
 import UpdateButton from './UpdateButton'
+import Image from 'next/image'
+import { CldUploadWidget } from 'next-cloudinary'
 
 export default function UpdateUser({ user }: { user: User }) {
 	const [open, setOpen] = useState(false)
+	const [cover, setCover] = useState<any>(false)
 
-	const [state] = useActionState(updateProfile, {
+	const [state, formAction] = useActionState(updateProfile, {
 		success: false,
 		error: false,
 	})
@@ -33,14 +36,44 @@ export default function UpdateUser({ user }: { user: User }) {
 			{open && (
 				<div className='absolute w-screen h-screen top-0 left-0 bg-black bg-opacity-65 flex items-center justify-center z-50 '>
 					<form
-						action=''
 						className='p-12 bg-white rounded-lg shadow-md flex flex-col gap-2 w-full md:w-1/2 xl:w-1/3 relative'
+						action={(formData) =>
+							formAction({ formData, cover: cover?.secure_url || '' })
+						}
 					>
 						{/* TITLE */}
 						<h1>Update Profile</h1>
 						<div className='mt-4 text-xs text-gray-500'>
 							Use the navbar profile to change the avatar or username.
 						</div>
+						{/* COVER PIC UPLOAD */}
+						<CldUploadWidget
+							uploadPreset='lama-socmed'
+							onSuccess={(result) => setCover(result.info)}
+						>
+							{({ open }) => {
+								return (
+									<div
+										className='flex flex-col gap-4 my-4'
+										onClick={() => open()}
+									>
+										<label htmlFor=''>Cover Picture</label>
+										<div className='flex items-center gap-2 cursor-pointer'>
+											<Image
+												src={user.cover || '/noCover.png'}
+												alt=''
+												width={48}
+												height={32}
+												className='w-12 h-8 rounded-md object-cover'
+											/>
+											<span className='text-xs underline text-gray-600'>
+												Change
+											</span>
+										</div>
+									</div>
+								)
+							}}
+						</CldUploadWidget>
 
 						{/* WRAPPER */}
 						<div className='flex flex-wrap justify-between gap-2 xl:gap-4'>
